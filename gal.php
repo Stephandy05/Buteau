94e1c8042d5726fd6446bba3fa540efb113a945d
Stephandy05 <88558981+Stephandy05@users.noreply.github.com>
2021-08-09 15:13:52 -0400

    Ajout

---
+++ gal.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Gallerie</title>
+	<link rel="stylesheet" type="text/css" href="css/menu.css">
+
+
+	<link rel="stylesheet" type="text/css" href="css/gal.css">
+	
+	
+</head>
+<body>
+
+
+
+<?php("include/menu.php");
+
+
+?>
+
+
+<div class="container">
+	<div class="card">
+		
+		<div class="imgbx">
+		   <img src="img/img1.jpeg">
+		</div>
+		   <div class="content">
+			  <h2> Card one </h2>
+			    <p> Depuis plusieurs décennies l'évaluation de l'apprentissage représente un aspect crucial dans le système éducatif haïtien. En fait, la question qui se pose aujourd'hui est;  comment évaluer des compétences ? D
+			    t être efficace, juste et équitable doit être fondé sur une évaluation formalisée et rigoureuse.</p>
+		    </div>
+	</div>
+	
+</div>
+
+<?php
+
+//*---------------------------------------------------------------------------------------------------------------
+?>
+
+<div class="container">
+	<div class="card">
+		
+		<div class="imgbx">
+		   <img src="img/img1.jpeg">
+		</div>
+		   <div class="content">
+			  <h2> Card one </h2>
+			    <p> Depuis plusieurs décennies l'évaluation de l'apprentissage représente un aspect crucial dans le système éducatif haïtien. En fait, la question qui se pose aujourd'hui est;  comment évaluer des compétences ? Dès lors, cette interrogation ne cause pas mal de problèmes. L'effet que les notions d'éducation et d'évaluation étant intimement liées, l'une à l'autre, celles-ci à une très grande importance. Un système éducatif se veut être efficace, juste et équitable doit être fondé sur une évaluation formalisée et rigoureuse.</p>
+		    </div>
+	</div>
+	
+</div>
+
+<?php
+
+//*---------------------------------------------------------------------------------------------------------------
+?>
+
+
+<div class="container">
+	<div class="card">
+		
+		<div class="imgbx">
+		   <img src="img/img1.jpeg">
+		</div>
+		   <div class="content">
+			  <h2> Card one </h2>
+			    <p> Depuis plusieurs décennies l'évaluation de l'apprentissage représente un aspect crucial dans le système éducatif haïtien. En fait, la question qui se pose aujourd'hui est;  comment évaluer des compétences ? Dès lors, cette interrogation ne cause pas mal de problèmes. L'effet que les notions d'éducation et d'évaluation étant intimement liées, l'une à l'autre, celles-ci à une très grande importance. Un système éducatif se veut être efficace, juste et équitable doit être fondé sur une évaluation formalisée et rigoureuse.</p>
+		    </div>
+	</div>
+	
+</div>
+
+
+
+<?php
+
+//*---------------------------------------------------------------------------------------------------------------
+?>
+
+
+<div class="container">
+	<div class="card">
+		
+		<div class="imgbx">
+		   <img src="img/img1.jpeg">
+		</div>
+		   <div class="content">
+			  <h2> Card one </h2>
+			    <p> Depuis plusieurs décennies l'évaluation de l'apprentissage représente un aspect crucial dans le système éducatif haïtien. En fait, la question qui se pose aujourd'hui est;  comment évaluer des compétences ? Dès lors, cette interrogation ne cause pas mal de problèmes. L'effet que les notions d'éducation et d'évaluation étant intimement liées, l'une à l'autre, celles-ci à une très grande importance. Un système éducatif se veut être efficace, juste et équitable doit être fondé sur une évaluation formalisée et rigoureuse.</p>
+		    </div>
+	</div>
+	
+</div>
+
+
+
+
+
+<?php
+
+//*---------------------------------------------------------------------------------------------------------------
+?>
+
+
+<div class="container">
+	<div class="card">
+		
+		<div class="imgbx">
+		   <img src="img/img1.jpeg">
+		</div>
+		   <div class="content">
+			  <h2> Card one </h2>
+			    <p> Depuis plusieurs décennies l'évaluation de l'apprentissage représente un aspect crucial dans le système éducatif haïtien. En fait, la question qui se pose aujourd'hui est;  comment évaluer des compétences ? Dès lors, cette interrogation ne cause pas mal de problèmes. L'effet que les notions d'éducation et d'évaluation étant intimement liées, l'une à l'autre, celles-ci à une très grande importance. Un système éducatif se veut être efficace, juste et équitable doit être fondé sur une évaluation formalisée et rigoureuse.</p>
+		    </div>
+	</div>
+	
+</div>
+
+
+
+
+
+<?php
+
+//*---------------------------------------------------------------------------------------------------------------
+?>
+
+
+<div class="container">
+	<div class="card">
+		
+		<div class="imgbx">
+		   <img src="img/img1.jpeg">
+		</div>
+		   <div class="content">
+			  <h2> Card one </h2>
+			    <p> Depuis plusieurs décennies l'évaluation de l'apprentissage représente un aspect crucial dans le système éducatif haïtien. En fait, la question qui se pose aujourd'hui est;  comment évaluer des compétences ? Dès lors, cette interrogation ne cause pas mal de problèmes. L'effet que les notions d'éducation et d'évaluation étant intimement liées, l'une à l'autre, celles-ci à une très grande importance. Un système éducatif se veut être efficace, juste et équitable doit être fondé sur une évaluation formalisée et rigoureuse.</p>
+		    </div>
+	</div>
+	
+</div>
+
+
+
+</body>
+</html>
